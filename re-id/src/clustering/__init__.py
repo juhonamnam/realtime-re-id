8df17b6cadf7e0800a2b0b7938b8bd94268039c8
juhonamnam <jhnamnam@gmail.com>
2026-05-04 19:26:53 +0900

    clustering verification [skip ci]

---
+++ re-id/src/clustering/__init__.py
@@ -0,0 +1,60 @@
+import torch
+
+__all__ = ['Person', 'cluster_people']
+
+class Person:
+    def __init__(self, v_scores: torch.Tensor, emb_vecs: torch.Tensor):
+        self.v_scores = v_scores
+        self.emb_vecs = emb_vecs
+        self.v_scores_sum = v_scores.sum().item()
+
+    def __str__(self):
+        return f"Person(v_scores={self.v_scores}, emb_vecs={self.emb_vecs})"
+
+class Cluster(Person):
+    def __init__(self, person: Person):
+        super().__init__(person.v_scores, person.emb_vecs)
+        self.members = [person]
+
+    def add_member(self, person: Person):
+        self.members.append(person)
+        for i in range(len(self.v_scores)):
+            if person.v_scores[i] > self.v_scores[i]:
+                self.v_scores[i] = person.v_scores[i]
+                self.emb_vecs[i] = person.emb_vecs[i]
+
+def get_comparability(query: Person, cluster: Cluster) -> float:
+    comparability = 0.0
+    for i in range(len(query.v_scores)):
+        comparability += min(query.v_scores[i], cluster.v_scores[i]).item()
+    return comparability
+
+def cluster_people(people: list[Person], threshold: float, similarity_func: callable):
+    labels = [0] * len(people)
+    clusters = []
+    sorted_people = sorted(map(lambda idx, person: (idx, person), range(len(people)), people),
+                           key=lambda x: x[1].v_scores_sum, reverse=True)
+    for person_idx, person in sorted_people:
+        best_cluster = None
+        best_similarity = -1
+        best_cluster_index = -1
+
+        for cluster_idx, cluster in enumerate(clusters):
+            similarity = similarity_func(person, cluster)
+            comparability = get_comparability(person, cluster)
+            if similarity < threshold or comparability < 0.5:
+                continue
+            if similarity > best_similarity:
+                best_similarity = similarity
+                best_cluster = cluster
+                best_cluster_index = cluster_idx
+
+        if best_cluster is not None:
+            best_cluster.add_member(person)
+            labels[person_idx] = best_cluster_index
+        else:
+            new_cluster = Cluster(person)
+            clusters.append(new_cluster)
+            labels[person_idx] = len(clusters) - 1
+
+    return labels
--- re-id/src/metric/__init__.py
+++ re-id/src/metric/__init__.py
@@ -6,6 +6,8 @@ from .weighted_mean import get_weighted_mean_similarity_score
 from .weighted_geometric_mean import get_weighted_geometric_mean_similarity_score
 from .minimum import get_minimum_similarity_score
 
+__all__ = ['SimilarityMetric']
+
 SIMILARITY_METRIC_REGISTRY = {
     'weakest_link': get_weakest_link_similarity_score,
     'weighted_mean': get_weighted_mean_similarity_score,
@@ -15,39 +17,56 @@ SIMILARITY_METRIC_REGISTRY = {
 
 SIMILARITY_METRICS = list(SIMILARITY_METRIC_REGISTRY.keys())
 
-class SimilarityMetric(nn.Module):
-    def __init__(self, method='weakest_link'):
+class SimilarityMetric():
+    similarity_metrics = SIMILARITY_METRICS
+    metric_num = len(similarity_metrics)
+    def __init__(self, default_method='weakest_link', default_part_thresholds=0.5):
         super(SimilarityMetric, self).__init__()
-        if method in SIMILARITY_METRIC_REGISTRY:
-            self.similarity_func = SIMILARITY_METRIC_REGISTRY[method]
+        if default_method in SIMILARITY_METRIC_REGISTRY:
+            self.default_similarity_func = SIMILARITY_METRIC_REGISTRY[default_method]
         else:
-            raise ValueError(f"Unsupported similarity metric method: {method}")
+            raise ValueError(f"Unsupported similarity metric method: {default_method}")
+        self.default_part_thresholds = default_part_thresholds
 
-    def forward(self, feature1, feature2, part_thresholds, return_part_scores=False):
+        self.metric_list = []
+        self.metric_dict = {}
+        for method in self.similarity_metrics:
+            self.metric_list.append(SIMILARITY_METRIC_REGISTRY[method])
+            self.metric_dict[method] = SIMILARITY_METRIC_REGISTRY[method]
+
+    def default(self, feature1, feature2, part_thresholds=None, return_part_scores=False):
+        if part_thresholds is None:
+            part_thresholds = self.default_part_thresholds
         v_scores, part_s_scores = prepare_for_similarity_metric(feature1, feature2)
         
-        total_s_score = self.similarity_func(v_scores, part_s_scores, part_thresholds)
+        total_s_score = self.default_similarity_func(v_scores, part_s_scores, part_thresholds)
         if return_part_scores:
             return total_s_score, part_s_scores
         return total_s_score
 
-class AllSimilarityMetrics(nn.Module):
-    similarity_metrics = SIMILARITY_METRICS
-    metric_num = len(similarity_metrics)
-    def __init__(self):
-        super(AllSimilarityMetrics, self).__init__()
-        self.metrics = []
-        for method in self.similarity_metrics:
-            self.metrics.append(SIMILARITY_METRIC_REGISTRY[method])
+    def metric(self, metric_name, feature1, feature2, part_thresholds=None):
+        if part_thresholds is None:
+            part_thresholds = self.default_part_thresholds
+        if metric_name not in self.metric_dict:
+            raise ValueError(f"Unsupported similarity metric method: {metric_name}")
+        similarity_func = self.metric_dict[metric_name]
+        v_scores, part_s_scores = prepare_for_similarity_metric(feature1, feature2)
+        total_s_score = similarity_func(v_scores, part_s_scores, part_thresholds)
+        return total_s_score
 
-    def forward(self, feature1, feature2, part_thresholds):
+    def all(self, feature1, feature2, part_thresholds=None):
+        if part_thresholds is None:
+            part_thresholds = self.default_part_thresholds
         v_scores, part_s_scores = prepare_for_similarity_metric(feature1, feature2)
 
         total_s_scores = []
 
-        for metric in self.metrics:
+        for metric in self.metric_list:
             total_s_score = metric(v_scores, part_s_scores, part_thresholds)
                 
             total_s_scores.append(total_s_score)
 
         return total_s_scores, part_s_scores
+
+    def __call__(self, feature1, feature2, part_thresholds=None, return_part_scores=False):
+        return self.default(feature1, feature2, part_thresholds, return_part_scores)
